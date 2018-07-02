05f563c19ca278a99e6cbfa8b929a703ff576eb3
scott <scott@tams.tech>
2018-07-02 01:24:43 -0400

    Revised more advanced tests to forward their cases to the Attest method

--- tests.go
+++ tests.go
@@ -46,19 +46,18 @@ func (t *Test) AttestOrDo(that bool, callback func(*Test)) {
 
 // AttestNil -- Log a message and fail if the variable is not nil
 func (t *Test) AttestNil(variable interface{}) {
-	if variable != nil {
-		log.Printf("%#v was expected to be nil, but was not!", variable)
-		t.NativeTest.Fail()
-	}
+	t.Attest(
+		variable == nil,
+		"%#v was expected to be nil, but was not!",
+		variable)
 }
 
 // AttestNotNil --  Log a message and fail if the variable is nil.
 func (t *Test) AttestNotNil(variable interface{}) {
-	if variable == nil {
-		log.Printf(
-			"%#v was expected to have a value but instead was nil", variable)
-		t.NativeTest.Fail()
-	}
+	t.Attest(
+		variable != nil,
+		"%#v was expected to have a value but instead was nil",
+		variable)
 }
 
 // AttestGreaterThan -- log a message and fail if the variable is less than the
@@ -73,42 +72,49 @@ func (t *Test) AttestGreaterThan(expected, variable interface{}) {
 			variable,
 			expected,
 			variable)
+		t.NativeTest.Fail()
+	case int:
+		t.Attest(
+			variable.(int) < expected.(int),
+			"Value (%#v) was less than expected (%#v).",
+			variable,
+			expected)
 	case int8:
-		if variable.(int8) < expected.(int8) {
-			log.Printf(
-				"Value (%#v) was less than expected (%#v).", variable, expected)
-			t.NativeTest.Fail()
-		}
+		t.Attest(
+			variable.(int8) < expected.(int8),
+			"Value (%#v) was less than expected (%#v).",
+			variable,
+			expected)
 	case int16:
-		if variable.(int16) < expected.(int16) {
-			log.Printf(
-				"Value (%#v) was less than expected (%#v).", variable, expected)
-			t.NativeTest.Fail()
-		}
+		t.Attest(
+			variable.(int16) < expected.(int16),
+			"Value (%#v) was less than expected (%#v).",
+			variable,
+			expected)
 	case int32:
-		if variable.(int32) < expected.(int32) {
-			log.Printf(
-				"Value (%#v) was less than expected (%#v).", variable, expected)
-			t.NativeTest.Fail()
-		}
+		t.Attest(
+			variable.(int32) < expected.(int32),
+			"Value (%#v) was less than expected (%#v).",
+			variable,
+			expected)
 	case int64:
-		if variable.(int64) < expected.(int64) {
-			log.Printf(
-				"Value (%#v) was less than expected (%#v).", variable, expected)
-			t.NativeTest.Fail()
-		}
+		t.Attest(
+			variable.(int64) < expected.(int64),
+			"Value (%#v) was less than expected (%#v).",
+			variable,
+			expected)
 	case float32:
-		if variable.(float32) < expected.(float32) {
-			log.Printf(
-				"Value (%#v) was less than expected (%#v).", variable, expected)
-			t.NativeTest.Fail()
-		}
+		t.Attest(
+			variable.(float32) < expected.(float32),
+			"Value (%#v) was less than expected (%#v).",
+			variable,
+			expected)
 	case float64:
-		if variable.(float64) < expected.(float64) {
-			log.Printf(
-				"Value (%#v) was less than expected (%#v).", variable, expected)
-			t.NativeTest.Fail()
-		}
+		t.Attest(
+			variable.(float64) < expected.(float64),
+			"Value (%#v) was less than expected (%#v).",
+			variable,
+			expected)
 	}
 	// can't use > on complex numbers for some reason.
 	// FIXME: implement GT/LT for complex64 and complex128
@@ -123,42 +129,42 @@ func (t *Test) AttestPositive(variable interface{}) {
 				"type %T",
 			variable,
 			variable)
+		t.NativeTest.Fail()
+	case int:
+		t.Attest(
+			variable.(int) > 0,
+			"Value (%#v) was not positive.",
+			variable)
 	case int8:
-		if variable.(int8) < 0 {
-			log.Printf(
-				"Value (%#v) was not positive.", variable)
-			t.NativeTest.Fail()
-		}
+		t.Attest(
+			variable.(int8) > 0,
+			"Value (%#v) was not positive.",
+			variable)
 	case int16:
-		if variable.(int16) < 0 {
-			log.Printf(
-				"Value (%#v) was not positive.", variable)
-			t.NativeTest.Fail()
-		}
+		t.Attest(
+			variable.(int16) > 0,
+			"Value (%#v) was not positive.",
+			variable)
 	case int32:
-		if variable.(int32) < 0 {
-			log.Printf(
-				"Value (%#v) was not positive.", variable)
-			t.NativeTest.Fail()
-		}
+		t.Attest(
+			variable.(int32) > 0,
+			"Value (%#v) was not positive.",
+			variable)
 	case int64:
-		if variable.(int64) < 0 {
-			log.Printf(
-				"Value (%#v) was not positive.", variable)
-			t.NativeTest.Fail()
-		}
+		t.Attest(
+			variable.(int64) > 0,
+			"Value (%#v) was not positive.",
+			variable)
 	case float32:
-		if variable.(float32) < 0 {
-			log.Printf(
-				"Value (%#v) was not positive.", variable)
-			t.NativeTest.Fail()
-		}
+		t.Attest(
+			variable.(float32) > 0,
+			"Value (%#v) was not positive.",
+			variable)
 	case float64:
-		if variable.(float64) < 0 {
-			log.Printf(
-				"Value (%#v) was not positive.", variable)
-			t.NativeTest.Fail()
-		}
+		t.Attest(
+			variable.(float64) > 0,
+			"Value (%#v) was not positive.",
+			variable)
 	}
 	// can't use > on complex numbers for some reason.
 	// FIXME: implement GT/LT for complex64 and complex128
