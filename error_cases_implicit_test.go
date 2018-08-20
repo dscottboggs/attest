864eb85a8b00046cfca3a051437e87530caf080b
Scott <scott@tams.tec>
2018-08-20 19:31:13 -0400

    Removed code which arbitrarily prints during test runs...
    also moved implementation tests into their own functions.

--- error_cases_implicit_test.go
+++ error_cases_implicit_test.go
@@ -1,32 +1,24 @@
 package attest
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestAttestPanics(t *testing.T) {
 	test := Test{t}
-	test.AttestPanics(func(a ...interface{}) { panic(a[0].(string)) }, "test panic")
-	defer func() {
-		r := recover()
-		test.Attest(r == nil, "Printing a word caused a panic.")
-	}()
-	fmt.Println("Test passed.")
+	test.AttestPanics(
+		func(a ...interface{}) { panic(a[0].(string)) },
+		"test panic",
+	)
 }
 
 func TestAttestNoPanic(t *testing.T) {
 	test := Test{t}
 	test.AttestNoPanic(
-		func(a ...interface{}) { fmt.Printf(a[0].(string)) },
-		"Test function shouldn't panic.\n")
-	// the following is an explicit test on the implementation, not an implicit
-	// test like the others.
-	defer func() {
-		r := recover()
-		test.Attest(r != nil, "Test error function didn't panic!")
-	}()
-	panic("test panic")
+		func(a ...interface{}) { a[0] = a[1] },
+		"args for",
+		"callback func",
+	)
 }
 
 // this function is called by EatError, it returns a value and a string
@@ -37,4 +29,23 @@ func returnsError() (string, error) {
 func TestEatError(t *testing.T) {
 	test := Test{t}
 	test.Equals("success", test.EatError(returnsError()).(string))
+}
+
+// the following are explicit tests on the implementation, not implicit tests
+// like the others.
+func TestPanicCheckImplementationWithPanic(t *testing.T) {
+	test := NewTest(t)
+	defer func() {
+		r := recover()
+		test.Attest(r != nil, "Test error function didn't panic!")
+	}()
+	panic("test panic")
+}
+func TestPanicCheckImplementationWithNoPanic(t *testing.T) {
+	test := NewTest(t)
+	defer func() {
+		r := recover()
+		test.Attest(r == nil, "Basic math caused a panic.")
+	}()
+	_ = 2 + 2
 }
